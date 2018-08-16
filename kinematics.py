c395590f129d9d8b67a3a0ef8274470d5f52220f
zhujun98 <zhujun981661@gmail.com>
2018-08-16 18:21:46 +0200

    Change the coordinate and enable full 3D rotation.
    - Comparing with the conventional Tri-sphere coordinate system:
    y->z, z->y.
    - Rotation about x->y->z produces correct result.

--- kinematics.py
+++ kinematics.py
@@ -28,75 +28,87 @@ def rotation_fixed_angle(gamma, beta, alpha):
     return rotation_z(alpha) * rotation_y(beta) * rotation_x(gamma)
 
 
-def trisphere_forward_kinematics(j_dp, j_p0, c_p0):
+def trisphere_forward_kinematics(j_p, j_np, c_np,
+                                 c_p0=np.zeros(3),
+                                 c_r0=np.zeros(3)):
     """Forward kinematics of the Tri-sphere 6D system.
 
-    :param tuple: changes of the jacks' positions
-        (j1_dp, j2_dp, j3_dp)
-    :param tuple j_p0: current positions of the jacks
-        (j1_p0, j2_p0, j3_p0) in the Tri-Sphere coordinate system.
+    :param tuple: jacks' target positions (j1_p, j2_p, j3_p) relative to
+        their respective null positions.
+    :param tuple j_np: null positions of the jacks (j1_np, j2_np, j3_np)
+        in the Tri-Sphere coordinate system.
+    :param numpy.array c_np: null position of the control point in the
+        Tri-Sphere coordinate system.
     :param numpy.array c_p0: current position of the control point in the
         Tri-Sphere coordinate system.
+    :param numpy.array c_r0: current position of the control point in the
+        Tri-Sphere coordinate system.
 
-    :return: changes of the position (dx, dy, dz) and orientation
-        (gamma, beta, alpha) of the control point.
+    :return: target of the position (x, y, z) and rotation
+        (gamma, beta, alpha) of the control point in radians.
     """
     def obj_func(x):
-        ret, _ = trisphere_inverse_kinematics(x[:3], x[3:], c_p0, j_p0)
-        return np.array([ret[0][1] - j_dp[0][1], ret[0][2] - j_dp[0][2],
-                         ret[1][0] - j_dp[1][0], ret[1][2] - j_dp[1][2],
-                         ret[2][0] - j_dp[2][0], ret[2][2] - j_dp[2][2]])
+        ret, _ = trisphere_inverse_kinematics(x[:3], x[3:], c_np, j_np)
+        # z1, y1, x2, y2, x3, y3
+        return np.array([ret[0][2] - j_p[0][2], ret[0][1] - j_p[0][1],
+                         ret[1][0] - j_p[1][0], ret[1][1] - j_p[1][1],
+                         ret[2][0] - j_p[2][0], ret[2][1] - j_p[2][1]])
 
-    x0 = np.array([c_p0[0], c_p0[1], c_p0[2], 0., 0., 0.])
+    x0 = np.concatenate((c_p0, c_r0))
     ret = fsolve(obj_func, x0)
-    ret[3:] = np.rad2deg(ret[3:])
-    return ret
+    # c_p and c_r
+    return ret[:3], ret[3:]
 
 
-def trisphere_inverse_kinematics(c_dp, c_dr, c_p0, j_p0):
+def trisphere_inverse_kinematics(c_p, c_r, c_np, j_np):
     """Inverse kinematics of the Tri-sphere 6D system.
 
-    :param numpy.array c_dp: position change (dx, dy, dz) of the control
-        point.
-    :param numpy.array c_dr: orientation change (gamma, beta, alpha) of
-        the control point.
-    :param numpy.array c_p0: current position of the control point in the
+    :param numpy.array c_p: target position (x, y, z) of the control point
+        relative to its null position.
+    :param numpy.array c_r: target rotation (gamma, beta, alpha) of the
+        control point in radians.
+    :param numpy.array c_np: null position of the control point in the
         Tri-Sphere coordinate system.
-    :param tuple j_p0: current positions of the jacks
-        (j1_p0, j2_p0, j3_p0) in the Tri-Sphere coordinate system.
+    :param tuple j_np: null positions of the jacks (j1_np, j2_np, j3_np)
+        in the Tri-Sphere coordinate system.
 
-    :return: changes of the jacks' positions (j1_dp, j2_dp, j3_dp) and
-        "transformed current positions" (j1_tp0, j2_tp0, j3_tp0). The
-        latter is used for testing and debugging.
+    :return: jacks' target positions (j1_p, j2_p, j3_p) relative to their
+        respective null position and "transformed null positions"
+        (j1_tnp, j2_tnp, j3_tnp). The latter is used for testing and
+        debugging.
     """
-    j1_p0, j2_p0, j3_p0 = j_p0
-    gamma, beta, alpha = c_dr
+    j1_np, j2_np, j3_np = j_np
+    gamma, beta, alpha = c_r
 
-    # calculate overall rotation matrix
+    # calculate rotation matrix
     rt_matrix = rotation_fixed_angle(gamma, beta, alpha)
 
-    # calculate transformed current positions
-    j1_tp0 = ((j1_p0 - c_p0) * rt_matrix.T).getA()[0] + c_p0 + c_dp
-    j2_tp0 = ((j2_p0 - c_p0) * rt_matrix.T).getA()[0] + c_p0 + c_dp
-    j3_tp0 = ((j3_p0 - c_p0) * rt_matrix.T).getA()[0] + c_p0 + c_dp
+    # calculate transformed null positions
+    j1_tnp = ((j1_np - c_np) * rt_matrix.T).getA()[0] + c_np + c_p
+    j2_tnp = ((j2_np - c_np) * rt_matrix.T).getA()[0] + c_np + c_p
+    j3_tnp = ((j3_np - c_np) * rt_matrix.T).getA()[0] + c_np + c_p
 
     # calculate jacks' new positions
-    j1_dp = j1_tp0 - j1_p0
-    dx = j1_p0[0] - j1_tp0[0]
-    j1_dp[0] = 0
-    j1_dp[1] += np.tan(alpha) * dx
-    j1_dp[2] += -np.tan(beta)/np.cos(alpha) * dx
+    j1_p = j1_tnp - j1_np
+    dx = j1_np[0] - j1_tnp[0]
+    j1_p[0] = 0
+    j1_p[1] += np.tan(alpha) * dx
+    j1_p[2] += -np.tan(beta)/np.cos(alpha) * dx
 
-    j2_dp = j2_tp0 - j2_p0
-    dy = j2_p0[1] - j2_tp0[1]
-    j2_dp[0] += -(np.tan(alpha) - np.sin(beta)*np.tan(gamma)) * dy
-    j2_dp[1] = 0
-    j2_dp[2] += (np.tan(gamma)/np.cos(alpha) - np.sin(beta)**2*np.sin(gamma))*dy
+    j2_p = j2_tnp - j2_np
+    dz = j2_np[2] - j2_tnp[2]
+    j2_p[0] += (np.tan(beta)*np.cos(alpha)
+                + np.sin(alpha)*np.tan(gamma)/np.cos(beta)) * dz
+    j2_p[1] += -(np.tan(gamma)/np.cos(beta)*np.cos(alpha)
+                 - np.tan(beta)*np.sin(alpha)) * dz
+    j2_p[2] = 0
 
-    j3_dp = j3_tp0 - j3_p0
-    dy = j3_p0[1] - j3_tp0[1]
-    j3_dp[0] += -(np.tan(alpha) - np.sin(beta)*np.tan(gamma)) * dy
-    j3_dp[1] = 0
-    j3_dp[2] += (np.tan(gamma)/np.cos(alpha) - np.sin(beta)**2*np.sin(gamma))*dy
+    j3_p = j3_tnp - j3_np
+    dz = j3_np[2] - j3_tnp[2]
+    j3_p[0] += (np.tan(beta)*np.cos(alpha)
+                + np.sin(alpha)*np.tan(gamma)/np.cos(beta)) * dz
+    j3_p[1] += -(np.tan(gamma)/np.cos(beta)*np.cos(alpha)
+                 - np.tan(beta)*np.sin(alpha)) * dz
+    j3_p[2] = 0
 
-    return (j1_dp, j2_dp, j3_dp), (j1_tp0, j2_tp0, j3_tp0)
+    return (j1_p, j2_p, j3_p), (j1_tnp, j2_tnp, j3_tnp)
